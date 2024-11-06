600ba12bec67d52d1a3367c2699e17f9d2647a02
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2024-11-06 21:06:40 +0200

    cleaned up the code to a default expo app that runs on all systems
    We need to rewrite the coded and hunt down the modules that are obsolete.
    Going to start with a simple ssl connection to webrtc, ice and stun servers
    Here's a list of free to use ice and stun servers
    https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b

---
+++ Example-code-screens-and-app/App.js
@@ -0,0 +1,48 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Appbar, Menu } from 'react-native-paper';
+import { getHeaderTitle } from '@react-navigation/elements';
+import { CustomNavigationBar } from './components/CustomNavigationBar';
+const Stack = createStackNavigator();
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          header: (props) => <CustomNavigationBar {...props} />,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
